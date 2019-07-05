7893555458d201a13dada264cc756d93aae31f18
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-06 00:16:59 +0530

    New Project Added

---
+++ ShowNameNow/app/src/main/java/com/example/shownamenow/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.shownamenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView showText;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showText.setText("Hello " + editText.getText().toString());
+            }
+        });
+        showText = findViewById(R.id.textView);
+    }
+}
