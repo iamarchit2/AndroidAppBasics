1d84f4c23a13da4af5cf8cb932b574dc768f278e
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-09 11:39:37 +0530

    Changed MyClass Java File

--- ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -1,16 +1,27 @@
 package com.example.learnjava;
 
 public class MyClass {
-    public static void main(String[] args) { //1. main entry point.
-        //2. String variables
+    public static void main(String[] args) { //8. main entry point.
+        //9. String variables
         String name = "Archit";
-        //3. int variables
+
+        //10. int variables
         int age = 23;
-        // 4. long, float variables
+
+        // 11. long, float, double variables
         long weightOfPlanet = 369999990;
         float pi = 3.14f; // put f ahead of a float
-        double pid = 3.14d; // put d ahead of the double, they are bigger and more precise.
-        System.out.println("My name is " + name + " and my age is " + age + "." + " " + pi + " " + pid); // 3. concatenation
-        System.out.println("Hey, I am a Programmer.");
+        double pid = 3.14d; // put d ahead of the double, they are bigger and more precise
+
+        // 12. byte, short, char variable
+        byte b = 1; //can hold up to 127
+        short s = 156; //can hold up to 256
+        char c = '4';
+
+        // 13. boolean variables
+        boolean istrue = true;
+
+        System.out.println(istrue);
+        System.out.println("Hey, I am a Programmer." + "\nMy age is " + 23 + "."); // 10. concatenation
     }
 }
