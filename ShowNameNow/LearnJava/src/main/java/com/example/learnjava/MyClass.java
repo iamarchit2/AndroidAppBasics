3fbeadfab27818ca157e7311b0a0946b01ef37b6
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-09 11:58:31 +0530

    Changed MyClass Java File

--- ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClass.java
+++ ShowNameNow/LearnJava/src/main/java/com/example/learnjava/MyClass.java
@@ -19,9 +19,28 @@ public class MyClass {
         char c = '4';
 
         // 13. boolean variables
-        boolean istrue = true;
+        boolean istrue = false;
 
-        System.out.println(istrue);
+        //14. add, sub, mul, div
+        int p = 12;
+        double q = 14;
+        int add = (int) (p + q);
+        int sub = (int) (p - q);
+        int mul = (int) (p * q);
+        int div = (int) (p / q); //explicit casting
+
+        //15. Why use variables
+
+        //16. % operator, remainder
+        double remainder = (q % p);
+
+        //17. ==, != operator(relation operators)
+        //17. !(negation operator)
+
+        //18. <, >, <=, >=(relational operators)
+
+
+        System.out.println(remainder);
         System.out.println("Hey, I am a Programmer." + "\nMy age is " + 23 + "."); // 10. concatenation
     }
 }
