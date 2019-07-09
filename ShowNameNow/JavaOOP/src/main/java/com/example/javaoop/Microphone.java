5b9b154ac37aa0b8a7c8b920d724f5a025440dee
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-10 00:36:36 +0530

    Created Microphone Class

---
+++ ShowNameNow/JavaOOP/src/main/java/com/example/javaoop/Microphone.java
@@ -0,0 +1,8 @@
+package com.example.javaoop;
+
+public class Microphone { //modifier(public)
+    //instance variables, properties
+    String name;
+    String color;
+    int model;
+}
--- ShowNameNow/JavaOOP/src/main/java/com/example/javaoop/MyClass.java
+++ ShowNameNow/JavaOOP/src/main/java/com/example/javaoop/MyClass.java
@@ -1,4 +1,17 @@
 package com.example.javaoop;
 
 public class MyClass {
+    public static void main(String[] args) {
+        Microphone mic1 = new Microphone(); //instantiate using constructor
+        mic1.model = 12254;
+        mic1.color = "blue";
+        mic1.name = "blue yeti";
+        System.out.println(mic1); //only prints the address at which it is created
+
+        Microphone mic2 = new Microphone();
+        mic2.model = 134562;
+        mic2.color = "black";
+        mic2.name = "black bose";
+        System.out.println(mic2);
+    }
 }
