f6f14bce80c29b381e78c14f92b7804736aaf66f
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-11 00:33:38 +0530

    Added Person Manager and Employee Classes

---
+++ ShowNameNow/JavaOOP/src/main/java/com/example/javaoop/Manager.java
@@ -0,0 +1,14 @@
+package com.example.javaoop;
+
+public class Manager extends Employee {
+
+    @Override
+    public double getAnnualSalary() {
+        return super.getAnnualSalary() + 10000;
+    }
+
+    @Override
+    public int getId() {
+        return super.getId() + 100;
+    }
+}
--- ShowNameNow/JavaOOP/src/main/java/com/example/javaoop/MyClass.java
+++ ShowNameNow/JavaOOP/src/main/java/com/example/javaoop/MyClass.java
@@ -2,18 +2,18 @@ package com.example.javaoop;
 
 public class MyClass {
     public static void main(String[] args) {
-        Microphone mic1 = new Microphone("blue yeti", "blue",122 ); //instantiate using constructor
+        /*Microphone mic1 = new Microphone("blue yeti", "blue",122 ); //instantiate using constructor
         // constructor creates a spot in the memory
-        /*mic1.model = 12254;
+        *//*mic1.model = 12254;
         mic1.color = "blue";
         mic1.name = "blue yeti";
-        System.out.println(mic1);*/ //only prints the address at which it is created
+        System.out.println(mic1);*//* //only prints the address at which it is created
 
         Microphone mic2 = new Microphone("black bose", "black", 14258);
-        /*mic2.model = 134562;
+        *//*mic2.model = 134562;
         mic2.color = "black";
         mic2.name = "black bose";
-        System.out.println(mic2);*/
+        System.out.println(mic2);*//*
         mic1.turnON();
         mic1.setVolume();
         mic1.turnOFF();
@@ -24,7 +24,26 @@ public class MyClass {
         mic1.setModel(123);
         System.out.println(mic1.showDescription());
 
-        Microphone mic3 = new Microphone();
+        Microphone mic3 = new Microphone();*/
+
+
+
+        Person james = new Person("james", "bond", 23);
+
+        Employee jimmy = new Employee();
+        jimmy.setFirstname("jimmy");
+        jimmy.setLastname("kane");
+        jimmy.setAge(22);
+        jimmy.setId(12);
+        System.out.println(jimmy.getId());
+
+        Manager manager = new Manager();
+        manager.setFirstname("Joy");
+        manager.setLastname("Bishu");
+        manager.setAge(22);
+        manager.setId(1);
+        manager.setAnnualSalary(100000);
+        System.out.println(manager.getId());
 
     }
 }
