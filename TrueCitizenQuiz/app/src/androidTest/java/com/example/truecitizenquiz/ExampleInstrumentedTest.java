f42b895dae177b12d62691a92160cdb69bfeafb1
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-21 03:39:07 +0530

    True Citizen Quiz Modified

--- TrueCitizenQuiz/app/src/androidTest/java/com/example/truecitizenquiz/ExampleInstrumentedTest.java
+++ TrueCitizenQuiz/app/src/androidTest/java/com/example/truecitizenquiz/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.truecitizenquiz;
 
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
--- TrueCitizenQuiz/app/src/main/java/com/example/truecitizenquiz/MainActivity.java
+++ TrueCitizenQuiz/app/src/main/java/com/example/truecitizenquiz/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.truecitizenquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button trueButton;
+    private Button falseButton;
+    private TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.button_true);
+        falseButton = findViewById(R.id.button_false);
+        question = findViewById(R.id.textView_question);
+
+        /*falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*/
+
+        falseButton.setOnClickListener(this); //register our  buttons to listen to OnClickEvents
+        trueButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.button_false :
+                Toast.makeText(getApplicationContext(), "False", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_true :
+                Toast.makeText(getApplicationContext(), "True", Toast.LENGTH_SHORT).show();
+                break;
+                default: break;
+        }
     }
 }
