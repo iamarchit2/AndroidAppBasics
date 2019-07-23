7a128f7de9861370205113f09943a5411868ded5
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-24 02:54:11 +0530

    Overrided Methods

--- ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG", "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TAG", "onStart: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("TAG", "onPostResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TAG", "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("TAG", "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TAG", "onDestroy: ");
     }
 }
