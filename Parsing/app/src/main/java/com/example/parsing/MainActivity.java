20dff44c94066f1cb44afe67369f5c5b4e66cdf0
iamarchit2 <architaggarwal0508@gmail.com>
2019-07-28 23:43:05 +0530

    Parsed JSON Array

--- Parsing/app/src/main/java/com/example/parsing/MainActivity.java
+++ Parsing/app/src/main/java/com/example/parsing/MainActivity.java
@@ -7,9 +7,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("JSON", "onResponse: " + response);
+                        try {
+                            Log.d("JSON", "onResponse: " + response.getString("title"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
@@ -39,6 +46,32 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("JSON", "onErrorResponse: " + error);
             }
         });
-        requestQueue.add(jsonObjectRequest);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Log.d("TAG", "onResponse: " + response);
+                        for(int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                Log.d("TAG", "onResponse: " + jsonObject.getString("title") + " " + jsonObject.getString("id"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
